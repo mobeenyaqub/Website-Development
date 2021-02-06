5a52d80bc6ac51abfcdfee0ca94086cf7516a3b1
Mubeen Yaqub <70191073+mobeenyaqub@users.noreply.github.com>
2021-02-06 21:58:04 +0500

    Add files via upload

---
+++ backgroundColorChanger.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+
+    const button = document.querySelector("#button");
+    const body = document.querySelector("body");
+
+    button.addEventListener("click", () => {
+
+        let r = Math.random() * 255;
+        let g = Math.random() * 255;
+        let b = Math.random() * 255;
+
+        let styling = body.style.backgroundColor = `rgb(${r},${g},${b})`;
+        
+    })
+
+}
